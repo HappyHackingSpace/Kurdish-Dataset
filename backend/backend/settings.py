f44c202c896f04ce55b4eaf12071d815058c80b4
Baran Yalçın <98966968+baranylcn@users.noreply.github.com>
2025-05-26 20:52:04 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -5,13 +5,9 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'your-secret-key'
-
-# SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = []
