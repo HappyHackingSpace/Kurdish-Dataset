446bf44d8d6889211c9bdc9007bf27bde5982f2a
baranylcn <bbrnylcn@gmail.com>
2025-05-27 00:45:52 +0300

    Add Heroku domain to ALLOWED_HOSTS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,9 +8,10 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY")
+
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kurdish-dataset-44c0b09542cb.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
